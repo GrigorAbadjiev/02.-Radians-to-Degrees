d582f8502384e80524422d7f33fac939d2e6c7e7
nanji <nanji@DESKTOP-4A8MIDK>
2022-10-30 11:35:39 +0200

    Add project files.

---
+++ 02. Radians to Degrees/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //input
+           double angleInRadians = double.Parse(Console.ReadLine());
+            // calculate
+            Double angleInDegrees = angleInRadians * 180 / Math.PI;
+            // output
+            Console.WriteLine(angleInDegrees);
+        }
+    }
+}
